f48b7f97b8392df532e40151fc8a44c5ba119f21
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2020-11-23 18:49:08 +0800

    Add support for distance calculations

--- lib/battle_map.rb
+++ lib/battle_map.rb
@@ -2,8 +2,9 @@ class BattleMap
   def initialize(session, map_file)
     @session = session
     @map_file = map_file
+    @entities = {}
 
-    @properties = YAML.load_file(File.join(File.dirname(__FILE__), "..", "maps", "#{map_file}.yml")).deep_symbolize_keys!
+    @properties = YAML.load_file(File.join(File.dirname(__FILE__), "..", "#{map_file}.yml")).deep_symbolize_keys!
 
     @base_map = @properties.dig(:map, :base).map do |lines|
       lines.each_char.map.to_a
@@ -21,11 +22,20 @@ class BattleMap
 
   def place(pos_x, pos_y, entity, token = nil)
     @tokens[pos_x][pos_y] = { entity: entity, token: token || entity.name.first }
+    @entities[entity] = [pos_x, pos_y]
+  end
+
+  def distance(entity1, entity2)
+    pos1_x, pos1_y = @entities[entity1]
+    pos2_x, pos2_y = @entities[entity2]
+
+    Math.sqrt((pos1_x - pos2_x) ** 2 + (pos1_y - pos2_y) ** 2).ceil
   end
 
   def render
     @base_map.each_with_index.map do |row, row_index|
       row.each_with_index.map do |c, col_index|
+        c = "·" if c == "."
         # render map layer
         token = @tokens[row_index][col_index] ? @tokens[row_index][col_index][:token] : nil
         token || c
--- spec/battle_map_spec.rb
+++ spec/battle_map_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe AttackAction do
   let(:session) { Session.new }
   before do
-    @battle_map = BattleMap.new(session, "battle_sim")
+    @battle_map = BattleMap.new(session, "fixtures/battle_sim")
     @fighter = PlayerCharacter.load(File.join("fixtures", "high_elf_fighter.json"))
     @npc = Npc.new(:goblin, name: "grok")
     @battle_map.place(0, 1, @fighter)
@@ -12,17 +12,28 @@ RSpec.describe AttackAction do
   end
 
   specify "#render" do
-    expect(@battle_map.render).to eq ".G....\n" +
-                                     "...##.\n" +
-                                     "......\n" +
-                                     "......\n" +
-                                     ".##...\n" +
-                                     "......\n"
+    expect(@battle_map.render).to eq "·G····\n" +
+                                     "···##·\n" +
+                                     "······\n" +
+                                     "······\n" +
+                                     "·##···\n" +
+                                     "······\n"
   end
 
   context "#place" do
     specify "place tokens in the batlefield" do
       @battle_map.place(3, 3, @npc)
+      expect(@battle_map.render).to eq "·G····\n" +
+                                       "···##·\n" +
+                                       "······\n" +
+                                       "···g··\n" +
+                                       "·##···\n" +
+                                       "······\n"
     end
+  end
+
+  specify "#distance" do
+    @battle_map.place(3, 3, @npc)
+    expect(@battle_map.distance(@npc, @fighter)).to eq(4)
   end
 end
